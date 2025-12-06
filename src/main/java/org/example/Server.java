afb91c47b2d64a5624b26ba469e3e69a9b097f25
Noah Jutz <noah@noahjutz.com>
2025-12-06 23:07:27 +0100

    Add reflection

--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,10 +1,18 @@
 package org.example;
 
 import io.grpc.ServerBuilder;
+import io.grpc.protobuf.services.ProtoReflectionService;
+
+import java.io.IOException;
 
 class Server {
-  void main() throws InterruptedException {
-    final var server = ServerBuilder.forPort(8080).addService(new LidarWarningService()).build();
+  void main() throws InterruptedException, IOException {
+    final var server =
+        ServerBuilder.forPort(8080)
+            .addService(ProtoReflectionService.newInstance())
+            .addService(new LidarWarningService())
+            .build();
+    server.start();
     IO.println("Server running...");
     server.awaitTermination();
   }
